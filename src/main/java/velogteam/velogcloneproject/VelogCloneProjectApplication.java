cc488f818d40d648e1b7c9411b978970caa40138
ChoiHH03 <choihh0316@gmail.com>
2021-06-24 02:13:14 +0900

    Initial commit

---
+++ src/main/java/velogteam/velogcloneproject/VelogCloneProjectApplication.java
@@ -0,0 +1,13 @@
+package velogteam.velogcloneproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelogCloneProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VelogCloneProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/velogteam/velogcloneproject/VelogCloneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package velogteam.velogcloneproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelogCloneProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
